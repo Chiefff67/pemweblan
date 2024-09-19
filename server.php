1916ca28243c7dace306980a04ee3fa7b135f5d3
Chiefff67 <bocahrkr@gmail.com>
2024-09-19 14:59:11 +0800

    nambah alert

--- server.php
+++ server.php
@@ -1,9 +1,16 @@
 <?php
 $user = $_POST["user"];
 $pass = $_POST["pass"];
+
+// Memastikan input tidak kosong
+if (empty($user) || empty($pass)) {
+    echo "Username & Password Kosong!!";
+    exit;
+}
+
 if ($user == "admin") {
     if ($pass == "admin") {
-        echo "Login Berhasil";
+        echo "Berhasil";
     } else {
         echo "Password Salah";
     }
